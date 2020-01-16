b272027eff3b982f21a9cc0c98611bb5e507175b
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-16 11:19:55 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 var age = 29;
 var name = 'Anne';
+var favoriteFood = 'snakes';
 
 alert("Hello World!");
+
+document.write(favoriteFood);
+
+favoriteFood = 'ice cream';
+
+document.write(favoriteFood);
