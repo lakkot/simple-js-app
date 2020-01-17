388c83e8ce5a2ffe0c48df5e6f15f038de5e4a70
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-17 16:02:00 +0100

    add all pokemon objects to the array using one .push command

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,19 @@ var charmander = {
   height: 6,
   abilities: ['blaze', 'solar-power']
 }
-repository.push(charmander);
 
 var bulbasaur = {
   name: 'Bulbasaur',
   height: 7,
   abilities: ['chlorophyll', 'overgrow']
 }
-repository.push(bulbasaur);
 
 var squirtle = {
   name: 'Squirtle',
   height: 5,
   abilities: ['rain-dish', 'torrent']
 }
-repository.push(squirtle);
+repository.push(charmander, bulbasaur, squirtle);
 
 
 console.log(repository);
