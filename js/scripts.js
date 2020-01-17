d88c98729133b765bb5292357365f0b6f67f880d
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-17 10:26:44 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var age = 29;
 var name = 'Anne';
 var favoriteFood = 'snakes';
 
-alert("Hello World!");
+// alert("Hello World!");
 
 document.write(favoriteFood);
 
@@ -19,3 +19,19 @@ console.log(2+2);
 console.log('2'+'2');
 console.log('2'+2);
 console.log(parseInt('2')+2);
+
+
+
+var numberArray = [1, 2, 3];
+var mixedArray = [
+  1,
+  'two',
+  numberArray,
+  { age: 5 }
+];
+
+console.log(mixedArray[1]); // Outputs 'two'
+mixedArray[1] = 2;
+console.log(mixedArray[1]); // Outputs 2
+console.log(mixedArray[2]);
+console.log(numberArray.length); // will output 3
