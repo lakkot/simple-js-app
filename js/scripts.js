598561bdb0586ccb3a39845c6ed9cb847feba6e5
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-22 11:10:23 +0100

    trying out an if else statements
    one if else formula and one if-else combination for short formulas

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,17 @@ repository.push(charmander, bulbasaur, squirtle);
 
 
 console.log(repository);
+
+var age = 40;
+if (age <= 11) {
+  console.log('a child')
+} else if (age > 11 && age <20) {
+  console.log('a teenager')
+} else if (age >= 20 && age < 40) {
+  console.log('a young person')
+} else {
+  console.log('full grown')
+}
+
+var result = age >= 40 ? 'fully grown' : 'still young';
+console.log(result);
