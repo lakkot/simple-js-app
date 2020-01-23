7b9e0f7e5dbec6b6f0d91dbd97c1126c5bf0c43e
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-23 11:25:39 +0100

    function scope, return statement
    difference between return and 'non-return' function, scope of variables declared in function

--- js/scripts-learning.js
+++ js/scripts-learning.js
@@ -15,3 +15,31 @@ console.log(result);
 
 var ages = [20, 30, 25, 22, 31];
 console.log(ages.length);
+
+
+// a function without a return statement
+function add(number1, number2) {
+  console.log(number1 + number2);
+}
+var result = add(1, 2);
+console.log(result)
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+var result2 = multiply(3, 5);
+console.log(result2);
+
+
+//variable y will not be defined outside the foo function
+var x = 5;
+
+function foo(){
+  var y = 10;
+  console.log("From inside foo(): x = " + x);
+  console.log("From inside foo(): y = " + y);
+}
+
+foo();
+console.log("From outside foo(): x = " + x);
+console.log("From outside foo(): y = " + y);
