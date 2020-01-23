ef3db6e35af15cedfa54c3defe65b44e76a830c8
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-23 09:18:52 +0100

    adding second pokemon array and testing parameter function

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ var repository = [];
 
 var charmander = {
   name: 'Charmander',
-  height: 6,
+  height: 0.6,
   abilities: ['blaze', 'solar-power']
 }
 
 var bulbasaur = {
   name: 'Bulbasaur',
-  height: 7,
+  height: 0.7,
   abilities: ['chlorophyll', 'overgrow']
 }
 
 var squirtle = {
   name: 'Squirtle',
-  height: 5,
+  height: 0.5,
   abilities: ['rain-dish', 'torrent']
 }
 repository.push(charmander, bulbasaur, squirtle);
@@ -24,9 +24,42 @@ console.log(repository);
 
 
 for (i = 0; i < repository.length; i++) {
-  if (repository[i].height > 6) {
+  if (repository[i].height > 0.6) {
   document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + ') - Wow that\'s big! </p>');
-} else {
+  } else {
   document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + ') </p>');
+  }
 }
+
+// code like above, but writeen as function (in case you want to use it multiple times)
+function printArrayDetails() {
+  for (i = 0; i < repository.length; i++) {
+    if (repository[i].height > 0.6) {
+    document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + ') - Wow that\'s big! </p>');
+    } else {
+    document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + ') </p>');
+    }
+  }
 }
+
+printArrayDetails();
+
+// simmilar function, but with parameters in order to be used with multiple arrays
+var repository2 = [
+  {name: 'Pidgeot', height: 1.5, abilities: ['keen-eye', 'tangled-feet']},
+  {name: 'Vulpix', height: 0.6, abilities: ['flash-fire', 'drought']},
+  {name: 'Pikachu', height: 0.4, abilities: ['static', 'lightningrod']}
+];
+
+function printArrayDetails2(pokemonList) {
+  for (i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0.6) {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow that\'s big! </p>')
+    } else {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>')
+    }
+  }
+}
+
+printArrayDetails2(repository);
+printArrayDetails2(repository2);
