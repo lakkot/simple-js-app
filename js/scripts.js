8e9632a716a687e517ecef172b1943f4c21333d3
lakkot <40659615+lakkot@users.noreply.github.com>
2020-01-30 12:34:45 +0100

    fixing login webpage

--- js/scripts.js
+++ js/scripts.js
@@ -98,43 +98,24 @@ pokemonRepository.loadList().then(function() {
 });
 
 
+//-------validation of email and password--------
+//select input from html
+var $emailInput = document.querySelector('#emailinput');
+var $passwordInput = document.querySelector('#passwordinput');
 
+//validate email - must have @ and . sign
+function validateEmail() {
+  var value = $emailInput.value;
+  var hasAtSign = value.indexOf('@') > -1;
+  var hasDot = value.indexOf('.') > -1;
+  return value && hasAtSign && hasDot;
+}
 
+//validate password - must have 8 or more characters
+function validatePassword() {
+  var value = $passwordInput;
+  return value && value.length >= 8;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log();
+console.log(validateEmail());
+console.log(validatePassword());
